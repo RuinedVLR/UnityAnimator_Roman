11c8f8851368c229dc7726bcfd0892e07d5e6ed7
RuinedVLR <rzhurakhov@gmail.com>
2026-02-26 15:00:33 -0400

    final result

--- Assets/MegamanAnimController.cs
+++ Assets/MegamanAnimController.cs
@@ -7,20 +7,18 @@ public class MegamanAnimController : MonoBehaviour
     public Animator animController;
     public Vector2 moveInput;
 
+    int moveXHash = Animator.StringToHash("MoveInputX");
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         animController = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
+
+        animController.SetInteger("MoveInputX", (int)moveInput.x);
     }
 }
